df84f08a1482a1d8e147ac4b9e6b17a9de042047
Jack McKechnie <46594345+JackMcKechnie@users.noreply.github.com>
2024-04-19 12:06:43 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JM_PyTerrier_Utils',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    author='Jack McKechnie',
+    author_email='j.mckechnie.1@research.gla.ac.uk',
+    description='Commonly used PyTerrier utility functions.',
+    url='https://github.com/JackMcKechnie/JM_PyTerrier_Utils',
+)
